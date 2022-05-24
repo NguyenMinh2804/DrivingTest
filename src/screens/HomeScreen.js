9afede6a3be5c3a62b4baa48e4fdff95633e9e0a
Minh Nguyễn Văn <17520757@ms.uit.edu.vn>
2022-05-24 23:16:14 +0700

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const HomeCreen = () => {
+  return (
+    <View>
+      <Text>HomeCreen</Text>
+    </View>
+  );
+};
+
+
+export default HomeCreen;
+
+
---
+++ src/screens/TestScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const TestScreen = () => {
+  return (
+    <View>
+      <Text>TestScreen</Text>
+    </View>
+  );
+};
+
+
+export default TestScreen;
+
+
